ecf4228524ee76b660dc0dce453227d30117f40b
KY2001 <yukiyoshikein@gmail.com>
2022-08-14 15:47:48 +0900

    modify a bug

--- abc026/abc026_d_1653468525.cpp
+++ abc026/abc026_d_1653468525.cpp
@@ -71,48 +71,4 @@ signed main() {
             return 0;
         }
     }
-}</pre>
-
-			<h4>
-				Submission Info
-				
-			</h4>
-			
-			<div class="panel panel-default"><table class="table table-bordered table-striped">
-				<tr>
-					<th class="col-sm-4">Submission Time</th>
-					<td class="text-center"><time class='fixtime fixtime-second'>2022-05-25 17:48:45+0900</time></td>
-				</tr>
-				<tr>
-					<th>Task</th>
-					<td class="text-center"><a href="/contests/abc026/tasks/abc026_d">D - 高橋君ボール1号</a></td>
-				</tr>
-				<tr>
-					<th>User</th>
-					<td class="text-center"><a href="/users/KY2001">KY2001</a> <a href='/contests/abc026/submissions?f.User=KY2001'><span class='glyphicon glyphicon-search black' aria-hidden='true' data-toggle='tooltip' title='view ky's submissions'></span></a></td>
-				</tr>
-				<tr>
-					<th>Language</th>
-					<td class="text-center">C++ (GCC 9.2.1)</td>
-				</tr>
-				<tr>
-					<th>Score</th>
-					<td class="text-center">0</td>
-				</tr>
-				<tr>
-					<th>Code Size</th>
-					<td class="text-center">4094 Byte</td>
-				</tr>
-				<tr>
-					<th>Status</th>
-					<td id="judge-status" class="text-center"><span class='label label-warning' data-toggle='tooltip' data-placement='top' title="Compilation Error">CE</span></td>
-				</tr>
-				
-			</table></div>
-
-			
-				<h4>Compile Error</h4>
-				<pre>./Main.cpp: In function ‘int main()’:
-./Main.cpp:56:25: error: ‘R’ was not declared in this scope
-   56 |                 cout << R << endl;
-      |                         ^
+}
